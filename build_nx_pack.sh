9ee8bc77c40699bffd401187ce99dadd37d142b6
Alberto Quirós <74441259+SteadyEvenin@users.noreply.github.com>
2026-09-09 20:20:38 -0600

    Clarify sphaira output as a zip file
    Updated sphaira description to clarify the output format.

--- build_nx_pack.sh
+++ build_nx_pack.sh
@@ -418,7 +418,7 @@ process "Status-Monitor-Overlay" "ppkantorski/Status-Monitor-Overlay" "Status-Mo
 
 # 25. sphaira (NaGaa95 fork)
 # Actively maintained fork of the sphaira homebrew menu replacement.
-# Ships a standalone sphaira.nro placed at switch/sphaira/sphaira.nro.
+# Ships a zip containing sphaira.nro, placed at switch/sphaira/sphaira.nro.
 process "sphaira" "NaGaa95/sphaira" "sphaira.zip" "unzip_root"
 
 # 26. NXThemesInstaller
